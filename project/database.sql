d00aff604f0b157075204b8b91b1a33e4aa2d9a6
tszlongip <tszlongip@gmail.com>
2022-10-12 16:14:41 +0800

    update db

--- project/database.sql
+++ project/database.sql
@@ -3,6 +3,7 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
+    email VARCHAR(255),
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP DEFAULT NOW()
 );
