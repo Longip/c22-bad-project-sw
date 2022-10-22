8035e0cda126f10b6ffc46ce85b1b07017a0f851
tszlongip <tszlongip@gmail.com>
2022-10-22 14:29:40 +0800

    test yarn build

--- project/public/assets/user-profile.1cc49d84.js
+++ project/public/assets/user-profile.1cc49d84.js
@@ -1,21 +1,5 @@
 import "./modulepreload-polyfill.b7f2da20.js";
 var userProfile = "";
-let progressBar = document.querySelector(".circular-progress");
-let valueContainer = document.querySelector(".value-container");
-let progressValue = 0;
-let progressEndValue = 67;
-let speed = 20;
-let progress = setInterval(() => {
-  progressValue++;
-  valueContainer.textContent = `${progressValue}%`;
-  progressBar.style.background = `conic-gradient(
-      #4d5bf9 ${progressValue * 3.6}deg,
-      #cadcff ${progressValue * 3.6}deg
-  )`;
-  if (progressValue == progressEndValue) {
-    clearInterval(progress);
-  }
-}, speed);
 let button = document.querySelector(".toggle-button");
 let leftSideBarElem = document.querySelector(".left-sidebar");
 let newButton = document.querySelector(".new-toggle-button");
