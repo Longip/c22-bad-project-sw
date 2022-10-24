d1e12ed3baf49a04dee8eacc464cb4806eb49ea4
Mike Chan <kiwai0929@gmail.com>
2022-10-24 18:12:53 +0800

    successfully rename the camera image uploaded

--- project/utils/upload.ts
+++ project/utils/upload.ts
@@ -1,5 +1,5 @@
 import formidable, { Files } from "formidable"
-export const uploadDir = '../src/assets/uploads'
+export const uploadDir = 'uploads'
 import express from 'express';
 
 const form = formidable({
