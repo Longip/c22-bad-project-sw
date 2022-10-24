fe7ed15b5f96234392ddbcd687b3c7ff2bddc282
tszlongip <tszlongip@gmail.com>
2022-10-24 12:26:58 +0800

    merge homepage html

--- project/public/assets/homepage.3573d27a.js
+++ project/public/assets/homepage.3573d27a.js
@@ -35,7 +35,46 @@ window.addEventListener("load", async () => {
     console.log("Error: ", "please enable the location feature");
   }
 });
-async function logoutInit() {
+let button$1 = document.querySelector(".toggle-button");
+let leftSideBarElem$1 = document.querySelector(".left-sidebar");
+let newButton$1 = document.querySelector(".new-toggle-button");
+document.querySelector(".bar-function");
+let textElem$1 = document.querySelector(".text");
+let text1Elem$1 = document.querySelector(".text1");
+let text2Elem$1 = document.querySelector(".text2");
+let text3Elem$1 = document.querySelector(".text3");
+let text4Elem$1 = document.querySelector(".text4");
+let imageElem$1 = document.querySelector(".image-src > img");
+function leftSideBar$1() {
+  button$1 = document.querySelector(".toggle-button");
+  button$1.addEventListener("click", (e) => {
+    leftSideBarElem$1.style["max-width"] = "90px";
+    imageElem$1.style["width"] = "75px";
+    button$1.className = "new-toggle-button";
+    textElem$1.style.display = "none";
+    text1Elem$1.style.display = "none";
+    text2Elem$1.style.display = "none";
+    text3Elem$1.style.display = "none";
+    text4Elem$1.style.display = "none";
+    leftSideBar2$1();
+  });
+}
+function leftSideBar2$1() {
+  newButton$1 = document.querySelector(".new-toggle-button");
+  document.querySelector(".new-toggle-button").addEventListener("click", () => {
+    leftSideBarElem$1.style["max-width"] = "230px";
+    imageElem$1.style["width"] = "150px";
+    newButton$1.className = "toggle-button";
+    textElem$1.style.display = "block";
+    text1Elem$1.style.display = "block";
+    text2Elem$1.style.display = "block";
+    text3Elem$1.style.display = "block";
+    text4Elem$1.style.display = "block";
+    leftSideBar$1();
+  });
+}
+leftSideBar$1();
+async function logoutInit$1() {
   let logoutElem = document.querySelector(".logout-btn");
   logoutElem.addEventListener("click", async function(event) {
     event.preventDefault();
@@ -48,7 +87,7 @@ async function logoutInit() {
     }
   });
 }
-logoutInit();
+logoutInit$1();
 async function displayCard() {
   let res = await fetch("/restaurants/category");
   let cardDatas = (await res.json()).result;
@@ -120,3 +159,56 @@ displayCardElem.addEventListener("click", function() {
     cardContainer.style.display = "none";
   }
 });
+let button = document.querySelector(".toggle-button");
+let leftSideBarElem = document.querySelector(".left-sidebar");
+let newButton = document.querySelector(".new-toggle-button");
+document.querySelector(".bar-function");
+let textElem = document.querySelector(".text");
+let text1Elem = document.querySelector(".text1");
+let text2Elem = document.querySelector(".text2");
+let text3Elem = document.querySelector(".text3");
+let text4Elem = document.querySelector(".text4");
+let imageElem = document.querySelector(".image-src > img");
+function leftSideBar() {
+  button = document.querySelector(".toggle-button");
+  button.addEventListener("click", (e) => {
+    leftSideBarElem.style["max-width"] = "70px";
+    imageElem.style["width"] = "75px";
+    button.className = "new-toggle-button";
+    textElem.style.display = "none";
+    text1Elem.style.display = "none";
+    text2Elem.style.display = "none";
+    text3Elem.style.display = "none";
+    text4Elem.style.display = "none";
+    leftSideBar2();
+  });
+}
+function leftSideBar2() {
+  newButton = document.querySelector(".new-toggle-button");
+  document.querySelector(".new-toggle-button").addEventListener("click", () => {
+    leftSideBarElem.style["max-width"] = "230px";
+    imageElem.style["width"] = "150px";
+    newButton.className = "toggle-button";
+    textElem.style.display = "block";
+    text1Elem.style.display = "block";
+    text2Elem.style.display = "block";
+    text3Elem.style.display = "block";
+    text4Elem.style.display = "block";
+    leftSideBar();
+  });
+}
+leftSideBar();
+async function logoutInit() {
+  let logoutElem = document.querySelector(".logout-btn");
+  logoutElem.addEventListener("click", async function(event) {
+    event.preventDefault();
+    const res = await fetch("/user/logout", {
+      method: "POST"
+    });
+    console.log("test logout");
+    if (res.ok) {
+      location.replace("/index.html");
+    }
+  });
+}
+logoutInit();
